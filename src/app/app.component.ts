70d8b0a5ae90f156ddec177042346553c624d291
Caroline <wanguic49@gmail.com>
2023-09-10 22:45:32 +0300

    few changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { freeApiService } from './services/freeApi.service';
+import { Comments } from './classes/comments';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatTestApp';
+  listComments:Comments[] = [];
+
+  constructor(private _freeApiService:freeApiService){
+
+  }
+  
+  ngOnInit(){
+    this._freeApiService.getComments().subscribe(
+
+      data =>{
+        this.listComments = data;
+      }
+    );
+  }
 }
--- src/app/services/freeApi.service.ts
+++ src/app/services/freeApi.service.ts
@@ -1,6 +1,13 @@
 import {Injectable} from '@angular/core';
+import { Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
-export class freeApiService{
-    
+export class freeApiService
+{
+    constructor(private httpClient: HttpClient){}
+
+    getComments(): Observable<any>{
+        return this.httpClient.get("https://jsonplaceholder.typicode.com/comments");
+    }
 }
